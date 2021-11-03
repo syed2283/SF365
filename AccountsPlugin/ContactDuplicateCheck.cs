470aa0d41444c2c8430029ab393a2268d5e6e1a0
syed2283 <syed2283@outlook.com>
2021-11-03 17:54:03 +0530

    Initial commit

---
+++ AccountsPlugin/ContactDuplicateCheck.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
+namespace SF365.Plugins
+{
+    [CrmPluginRegistration(MessageNameEnum.Update,
+        "contact",
+        StageEnum.PreValidation,
+        ExecutionModeEnum.Synchronous, "fullname",
+        "Contact Duplicate-check",
+        1000,
+        IsolationModeEnum.Sandbox,
+        Image1Name = "PreImage",
+        Image1Type = ImageTypeEnum.PreImage,
+        Image1Attributes = "fullname")]
+
+    
+    public class ContactDuplicateCheck : PluginBase
+    {
+        public ContactDuplicateCheck() : base(typeof(ContactDuplicateCheck)) { }
+        protected override void ExecuteCrmPlugin(LocalPluginContext localcontext)
+        {
+            throw new InvalidPluginExecutionException("Working on it...!");
+        }
+        #region commented
+        /*
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Extract the tracing service for use in debugging sandboxed plug-ins.  
+            // If you are not registering the plug-in in the sandbox, then you do  
+            // not have to add any tracing service related code.  
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the organization service reference which you will need for  
+            // web service calls.  
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    // Plug-in business logic goes here.  
+                    //Read form attribute 
+                    string email = string.Empty;
+                    if (entity.Attributes.Contains("emailaddress1"))
+                        email = entity.Attributes["emailaddress1"].ToString();
+                    //select * from contact where emailaddress1==email
+                    QueryExpression query = new QueryExpression("contact");
+                    query.ColumnSet = new ColumnSet(new string[] { "emailaddress1" });
+                    query.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, email);
+                    if (service.RetrieveMultiple(query).Entities.Count > 0)
+                        throw new InvalidPluginExecutionException("Contact with email already exist!!!");
+
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+        */
+        #endregion
+    }
+}
---
+++ Webresources/SF365/js/accountScript.js
@@ -0,0 +1,64 @@
+﻿var Sdk = window.Sdk || {};
+(
+    function () {
+        this.formOnLoad = function (executionContext) {
+            //calling global helper file functions
+            Helper.DoSomething();
+
+            var formContext = executionContext.getFormContext();
+
+            //Form type example
+            var formtype = formContext.ui.getFormType();
+
+            if (formtype == 1)
+                formContext.ui.setFormNotification("User is creating Account", "INFO", "1");
+            else if (formtype == 2)
+                formContext.ui.setFormNotification("User is opening existing Account", "INFO", "2");
+            else if (formtype == 3)
+                formContext.ui.setFormNotification("User does not have permissions to edit the Account record", "INFO", "3");
+
+            //handling lookup values
+            //var lookupArray = formContext.getAttribute("parentcustomerid").getValue();
+
+            //if (lookupArray[0]!=null) {
+            //    var guidofAccount = lookupArray[0].id;
+            //    var nameofAccount = lookupArray[0].name;
+            //    var entitytype = lookupArray[0].entityType;
+
+            //    formContext.ui.setFormNotification("GUID of the Account is " + guidofAccount, "INFO", "1");
+            //    formContext.ui.setFormNotification("Name of the Account is " + nameofAccount, "INFO", "2");
+            //    formContext.ui.setFormNotification("Entity Type of the Account is " + entitytype, "INFO", "3");
+            //}
+
+            //var owndership = formContext.getAttribute("ownershipcode").getText();
+            //if (owndership) {
+            //    formContext.getControl("ownershipcode").setNotification(owndership,"ownershipmsg");
+            //} else {
+            //    formContext.getControl("ownershipcode").clearNotification("ownershipmsg");
+            //}
+        }
+        this.preventAutoSave = function (executionContext) {
+            var eventArgs = executionContext.getEventArgs();
+            if (eventArgs.getSaveMode() == 70) {
+                eventArgs.preventDefault();
+            }
+        }
+        this.MainPhoneOnChange = function (executionContext) {
+            var formContext = executionContext.getFormContext();
+            var phoneNumber = formContext.getAttribute("telephone1").getValue();
+
+            var expression = new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}");
+
+            if (!expression.test(phoneNumber)) {
+                //field notification
+                formContext.getControl("telephone1").setNotification("Enter Phone number in UAE format", "telephonemsg");
+                //form notification
+                formContext.ui.setFormNotification("Enter Phone number in UAE format", "INFO", "formnoti1");
+            } else {
+                //clear notification
+                formContext.getControl("telephone1").clearNotification("telephonemsg");
+                formContext.ui.clearFormNotification("formnoti1");
+            }
+        }
+    }
+).call(Sdk);
