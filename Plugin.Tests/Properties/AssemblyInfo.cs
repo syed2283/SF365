3d82b85b40bfba2fb7bac81dfa50ba02d192b973
syed2283 <syed2283@outlook.com>
2021-11-09 15:12:44 +0530

    Added test project

---
+++ Plugin.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plugin.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Plugin.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29e03472-c481-4bb9-8aa6-ce2e0d330ffa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Plugin.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Crm.Sdk.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SF365.Plugins;
+namespace Plugin.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var pipline = new PluginPipeline(FakeMessageNames.Create, FakeStages.PreOperation,
+                new Microsoft.Xrm.Sdk.Entity("contact")))
+            {
+                var plugin = new ContactDuplicateCheck();
+
+            }
+        }
+    }
+}
