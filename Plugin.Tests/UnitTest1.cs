0610ad2dcb2a3203a4812c47ce6412d16075ed22
syed2283 <syed2283@outlook.com>
2021-11-09 17:22:27 +0530

    added nom in webresources project and unittests folder for test

--- Plugin.Tests/UnitTest1.cs
+++ Plugin.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Crm.Sdk.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
 using SF365.Plugins;
 namespace Plugin.Tests
 {
@@ -8,13 +9,22 @@ namespace Plugin.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Unit Test")]
+        public void TestContactDuplicateCheck()
         {
             using (var pipline = new PluginPipeline(FakeMessageNames.Create, FakeStages.PreOperation,
                 new Microsoft.Xrm.Sdk.Entity("contact")))
             {
                 var plugin = new ContactDuplicateCheck();
-
+                try
+                {
+                    pipline.Execute(plugin);
+                    Assert.Fail("Exception not thrown");
+                }
+                catch (InvalidPluginExecutionException ex)
+                {
+                    Assert.IsTrue(ex.Message.Contains("Working on it"), "Must throw exeception");
+                }
             }
         }
     }
---
+++ Webresources/unittests/checkin-tests.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=checkin-tests.js.map
